2d60fc33d4a29babffc6b49bcc9e5e827cdcf984
Noel Rappin <noel.rappin@chime.com>
2024-05-20 12:40:03 -0500

    Revert "Test coverage, policy update page"
    This reverts commit 3a3c3e41f16a12ab4e5c9814e93df2fea34f7075.

--- app/views/users/_user.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! user, :id, :username, :password, :most_recent_login, :person_id, :created_at, :updated_at
-json.url user_url(user, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_17_204652) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_30_205612) do
   create_table "cards", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -42,17 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_17_204652) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.datetime "most_recent_login"
-    t.integer "person_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["person_id"], name: "index_users_on_person_id"
-  end
-
   add_foreign_key "cards", "people"
   add_foreign_key "cards", "projects"
-  add_foreign_key "users", "people"
 end
